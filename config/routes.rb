cd5063eeb448b2192304ca0a03c807a89b611943
Cale Sennett <calesennett@gmail.com>
2016-04-03 22:28:02 -0500

    start accounts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#index'
 
   resources 'digits', only: [:create]
+  resources 'users',  only: [:edit]
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 end
